bb35e25e9dd250ff0d6c9c3bd38138069285b024
Jordan Sokolic <oofnik@gmail.com>
2020-08-31 19:32:03 +0300

    disk parameter

--- qemu.sh
+++ qemu.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 PREFIX=${PREFIX:-/usr}
+DISK=$1
 qemu-system-x86_64 -enable-kvm -smp cpus=2 -m 256 \
   -M q35 -nographic \
   -netdev user,id=net0 -device e1000,netdev=net0,id=net0 \
   -netdev user,id=net1 -device e1000,netdev=net1,id=net1 \
   -drive if=pflash,format=raw,readonly,file="${PREFIX}/share/edk2-ovmf/x64/OVMF_CODE.fd" \
-  -drive format=raw,file=image.img
+  -drive format=raw,file=$DISK
